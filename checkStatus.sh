5273f568aaa0c1b6c0123a9e9e4b1e93ee58a404
Jorismaas <94828116+Jorismaas@users.noreply.github.com>
2022-07-07 10:27:10 +0200

    errors kunnen gevonden worden

--- checkStatus.sh
+++ checkStatus.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash 
 
-"" > $2.txt
+echo "" > $2.txt
+echo "" > $3.txt
 
 #twee >'s om ervoor te zorgen dat het onderop verschijnt
 #for loop
 #$1 $2
 for file in $(ls)
 do
-$1 $file 2>> $2.txt
+if $1 $file > /dev/null 2>> $2.txt
+then
+$1 $file 
+echo $file command uitgevoerd! >> $3.txt
+else
+2>> $2.txt
+echo $file command niet kunnen uitvoeren >> $3.txt
+fi
 done
